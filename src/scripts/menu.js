38e9cc7f7980ea3d80210e635eae971919d134c8
ecristoball <ecristoball@uoc.edu>
2024-12-16 19:17:58 +0100

    PEC2 version 0

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -1,6 +1,25 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const currentPage = window.location.pathname.split('/').pop();
-    if (currentPage === 'index.html' || currentPage === '') {
-        document.getElementById('inicio-link').style.display = 'none';
-    }
+    const checkLinks = () => {
+        const links = document.querySelectorAll('nav a'); // Selecciona los enlaces
+        console.log('Enlaces encontrados:', links); // Verifica si los enlaces están presentes
+
+        if (links.length > 0) { // Si los enlaces están presentes
+            const currentPage = window.location.pathname.split('/').pop() || 'index.html';
+            console.log('Página actual:', currentPage);
+
+            links.forEach(link => {
+                const linkHref = link.getAttribute('href');
+                console.log('Comprobando enlace:', linkHref);
+
+                if (linkHref.replace('/', '') === currentPage) {
+                    console.log(`Ocultando enlace: ${linkHref}`);
+                    link.style.display = 'none';
+                }
+            });
+        } else {
+            setTimeout(checkLinks, 100); // Reintenta si los enlaces no están listos
+        }
+    };
+
+    checkLinks();
 });
